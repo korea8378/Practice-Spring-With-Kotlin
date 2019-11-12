14ec39cbf2230e71e739e7fe1241bb5b20b0bd6d
YuChocopie <rladbwjd341@naver.com>
2019-11-13 03:03:51 +0900

    로컬서버로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("mysql:mysql-connector-java")
+//	implementation("mysql:mysql-connector-java:8.0.18")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
